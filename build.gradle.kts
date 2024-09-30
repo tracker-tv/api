7a9e65686cb73104ae88d4aec426a5ea5ae44b51
Florent Sorel <florent.sorel@gmail.com>
2024-10-01 00:17:24 +0200

    chore: add database connection

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,12 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+    // Database
+    implementation("org.jetbrains.exposed:exposed-spring-boot-starter:0.54.0")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.54.0")
+    implementation("org.liquibase:liquibase-core")
+    runtimeOnly("org.postgresql:postgresql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
